f7acbcbdc17788ec11b6b8e3c80b003bb995f907
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-09-09 14:08:23 +0200

    add basic recipe component & recipes state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React,{useEffect, useState } from 'react';
 import './App.css';
-
+import Recipe from './Recipe';
 const App = () =>{
   const APP_ID = "303aeecc";
   const APP_KEY = "b21661e5a989f5e5dfe869b91caf3f16";
 
+  const [recipes,setRecipes] = useState([]);
   
   useEffect(() => {
     getRecipes();
@@ -13,7 +14,7 @@ const App = () =>{
   const getRecipes = () => {
     fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => setRecipes(data.hits));
   }
   
   return(
@@ -22,6 +23,9 @@ const App = () =>{
           <input type="text" className="search-bar"/>
           <button type="submit" className="search-button">Search</button>
         </form>
+        {recipes.map(recipe=>(
+          <Recipe/> 
+        ))}
       </div>
   );
 }
