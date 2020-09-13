64f82c2227eed6ad46254128ebb1247177310150
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-09-13 20:41:26 +0200

    alter components

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,9 @@ import React from 'react';
 const Header = ({getSearch,search ,updateSearch}) =>{
     return(
         <div className="header">
+            <h1 className="logo">Cookary</h1>
             <form onSubmit={getSearch} className="search-form">
-            <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
+            <input type="text" className="search-bar" value={search} onChange={updateSearch} placeholder="Search with an ingredient"/>
             <button type="submit" className="search-button">
             Search
             </button>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const Recipe = ({title,calories,image,ingredients}) => {
-   return (
-    <div className="recipe-card">
-        <h1>{title}</h1>
-        <ul>
-            {ingredients.map(ingredient =>(
-                <li>{ingredient.text}</li>
-            ))}
-        </ul>
-        <p>{calories}</p>
-        <img src={image} alt=""/>
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className='recipe-card'>
+      <h1>{title}</h1>
+      <img src={image} alt="" />
+      <p>Calories: {calories}</p>
+      <h4>Ingredients</h4>
+      <ul className="ingredients-list">
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
     </div>
-   );
-}
+  );
+};
 
 export default Recipe;
